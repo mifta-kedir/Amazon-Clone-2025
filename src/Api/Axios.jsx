78f7d1ccbea34cd4abdc4a1fe4e36201895917d2
mifta-kedir <49817419+mifta-kedir@users.noreply.github.com>
2025-12-26 18:40:11 -0500

    second commit

--- src/Api/Axios.jsx
+++ src/Api/Axios.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 //for Backend Communication
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:5000", // for local host
-  baseURL: "http://127.0.0.1:5001/clone-11a1c/us-central1/api", // for cloud function deployment
+  // baseURL: "http://127.0.0.1:5001/clone-11a1c/us-central1/api",// for local host with functions
+  baseURL: "https://us-central1-clone-11a1c.cloudfunctions.net/api", // for cloud functions
 });
 
 export { axiosInstance };
